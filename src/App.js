4c68dfbb95d047a03f372e0f7802a20d9651171c
Vishal Kaushik <vishalkaushik@Vishals-MacBook-Pro.local>
2021-09-10 23:42:30 +0530

    Added components and corresponding scripts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import Todo from './components/Todo.js';
+import Modal from './components/Modal.js';
+import Backdrop from './components/Backdrop.js';
 
 function App() 
 {
   return (
     <div>
-      <h1> Hello </h1>
+      <h1>My Todos</h1>
+      <Todo text='Learn React' />
+      <Todo text='Master React' />
+      <Todo text='Explore the React course' />
+      <Modal />
+      <Backdrop />
     </div>
   );
 }
---
+++ src/components/Todo.js
@@ -0,0 +1,20 @@
+import {useState} from 'react';
+
+function Todo(props) {
+    useState();
+
+    function deleteHandler() {
+        console.log('Clicked!');
+        console.log(props.text);
+    }
+    return (
+        <div className='card'>
+            <h2>{props.text}</h2>
+            <div className='actions'>
+                <button className='btn' onClick={deleteHandler}>Delete</button>
+            </div>
+        </div>
+    );
+}
+
+export default Todo;
